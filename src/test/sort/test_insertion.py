ba5f5372821bf8e2bd79b6522d9d7008773213c1
Lucas Gonçalves Cordeiro <lucas.g.cordeiro@ufms.br>
2024-09-12 22:11:28 -0400

    test: enabled tests for insertion sort

--- src/test/sort/test_insertion.py
+++ src/test/sort/test_insertion.py
@@ -1,7 +1,6 @@
 import unittest
 from implex.sort import insertion
 
-@unittest.skip("Not implemented yet!")
 class TestInsertionSort(unittest.TestCase):
 
     def test_sort_descending_elements(self):
