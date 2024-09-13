ba5c71f3e3522c188bf426e078f49d6b4e78de6d
Lucas Gonçalves Cordeiro <lucas.g.cordeiro@ufms.br>
2024-09-12 20:30:27 -0400

    test: base test cases for sorting algorithms

---
+++ src/test/sort/test_heap.py
@@ -0,0 +1,43 @@
+import unittest
+from implex.sort import heap
+
+class TestHeapSort(unittest.TestCase):
+
+    def test_sort_descending_elements(self):
+        arr = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5]
+        sortedArr = heap.sort(arr)
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_ordered_elements(self):
+        arr = [-3, -2, -1, 0, 1, 2, 3]
+        sortedArr = heap.sort(arr)
+        expected = [-3, -2, -1, 0, 1, 2, 3]
+        self.assertEquals(sortedArr, expected)
+
+    def test_empty_list(self):
+        arr = []
+        sortedArr = heap.sort(arr)
+        expected = []
+        self.assertEquals(sortedArr, expected)
+    
+    def test_one_element(self):
+        arr = [0]
+        sortedArr = heap.sort(arr)
+        expected = [0]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_two_unordered_elements(self):
+        arr = [0, -1]
+        sortedArr = heap.sort(arr)
+        expected = [-1, 0]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_two_ordered_elements(self):
+        arr = [0, 1]
+        sortedArr = heap.sort(arr)
+        expected = [0, 1]
+        self.assertEquals(sortedArr, expected)
+
+if __name__ == "__main__":
+    unittest.main()
