ddf48f2f77e723a7197eef05b53e0652d33138cb
Lucas Kodex <lucas.g.cordeiro@ufms.br>
2024-09-12 22:03:49 -0400

    preparar projeto para tests com unittest (#9)
    * chore: renamed package
    
    * test: base test cases for sorting algorithms
    
    * docs: added test running instructions
    
    * chore: skipping tests of not implemented algorithms

---
+++ src/test/sort/test_merge.py
@@ -0,0 +1,44 @@
+import unittest
+from implex.sort import merge
+
+@unittest.skip("Not implemented yet!")
+class TestMergeSort(unittest.TestCase):
+
+    def test_sort_descending_elements(self):
+        arr = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5]
+        sortedArr = merge.sort(arr)
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_ordered_elements(self):
+        arr = [-3, -2, -1, 0, 1, 2, 3]
+        sortedArr = merge.sort(arr)
+        expected = [-3, -2, -1, 0, 1, 2, 3]
+        self.assertEquals(sortedArr, expected)
+
+    def test_empty_list(self):
+        arr = []
+        sortedArr = merge.sort(arr)
+        expected = []
+        self.assertEquals(sortedArr, expected)
+    
+    def test_one_element(self):
+        arr = [0]
+        sortedArr = merge.sort(arr)
+        expected = [0]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_two_unordered_elements(self):
+        arr = [0, -1]
+        sortedArr = merge.sort(arr)
+        expected = [-1, 0]
+        self.assertEquals(sortedArr, expected)
+    
+    def test_two_ordered_elements(self):
+        arr = [0, 1]
+        sortedArr = merge.sort(arr)
+        expected = [0, 1]
+        self.assertEquals(sortedArr, expected)
+
+if __name__ == "__main__":
+    unittest.main()
