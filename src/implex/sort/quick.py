12705c5066b3cae28a4ed28e3de8efca53a3c216
vOdajze <joseodatupi@gmail.com>
2024-09-16 00:38:02 -0300

    fix: resolve bug pivo

--- src/implex/sort/quick.py
+++ src/implex/sort/quick.py
@@ -1,3 +1,5 @@
+import random
+
 def sort(arr: list = list()) -> list:
     sortedArr = arr.copy()
     __quick_sort(sortedArr, 0, len(sortedArr) - 1)
@@ -6,14 +8,15 @@ def sort(arr: list = list()) -> list:
 def __quick_sort(arr: list, low: int, high: int):
     if low < high:
         pi = __partition(arr, low, high)
-
         __quick_sort(arr, low, pi - 1)
         __quick_sort(arr, pi + 1, high)
 
 def __partition(arr: list, low: int, high: int) -> int:
-    pivot = arr[high]  
-    i = low - 1 
+    pivot_index = random.randint(low, high)
+    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+    pivot = arr[high]
 
+    i = low - 1
     for j in range(low, high):
         if arr[j] < pivot:
             i += 1
